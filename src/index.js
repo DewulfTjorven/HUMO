b6412f0ee19f3152c97f5c9bb4276ec2cffaa028
DewulfTjorven <tjorven.dewulf@student.howest.be>
2020-01-09 20:13:47 +0100

    linked js en added navbar #1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 require('./css/reset.css');
 require('./css/style.css');
-require('./css/responsive.css');
 require('./css/vars.css');
 
+{
+  const init = () => {
+
+    console.log('logged');
+
+  };
+
+  init();
+}
--- src/view/layout.php
+++ src/view/layout.php
@@ -3,26 +3,19 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Flancer</title>
-    <?php /* NEW */ ?>
+    <title>HUMO</title>
     <?php echo $css;?>
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap" rel="stylesheet">
   </head>
   <body>
-    <main>
-      <?php
-
-        if(!empty($_SESSION['error'])) {
-          echo '<div class="error box">' . $_SESSION['error'] . '</div>';
-        }
-        if(!empty($_SESSION['info'])) {
-          echo '<div class="info box">' . $_SESSION['info'] . '</div>';
-        }
-
-      ?>
+    <main class="container">
+      <nav>
+        <img class="hamburger" src="../images/icons/nav.png">
+        <img class="humo--logo" src="../images/humo.png">
+        <img src="../images/icons/shopping-cart.png">
+      </nav>
       <?php echo $content;?>
     </main>
     <?php echo $js; ?>
-    <script src="js/validate.js"></script>
   </body>
 </html>
